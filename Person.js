085639ccf83379a7780cf9a161de521c9816e659
Michael Stallings <stall84@gmail.com>
2020-03-12 18:52:36 -0400

    started on villain.js

--- Person.js
+++ Person.js
@@ -2,18 +2,21 @@
 
 class Person {
 
-    constructor(name, health, weapon) {
+    // hard coded 100 health and no weapon for each person at start. name will be passed in as string 
+    constructor(name) {
         this.name = name;
         this.health = 100;
         this.weapon = null;
     }
 
+    // 
     equipWeapon(weapon) {
         this.weapon = weapon;
     }
 
+    // not sure about the math below. damage is a yet to be defined property. assuming that will come in the weapon.js file
     attack(person) {
-        person -= this.weapon;
+        person.health -= this.weapon.damage;
     }
 
 }
